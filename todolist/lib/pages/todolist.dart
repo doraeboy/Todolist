212538284f5fd02489b7a8e16473812f6de7a37d
doraeboy <89691433+doraeboy@users.noreply.github.com>
2021-10-30 23:38:57 +0700

    Add files via upload

---
+++ todolist/lib/pages/todolist.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/pages/add.dart';
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+import 'package:todolist/pages/update_todolist.dart';
+
+class Todolist extends StatefulWidget {
+  //const Todolist({ Key? key }) : super(key: key);
+
+  @override
+  _TodolistState createState() => _TodolistState();
+}
+
+class _TodolistState extends State<Todolist> {
+  List todolistitems = ['A', 'B', 'C', 'D'];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getTodolist();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddPage())).then((value) {
+                            setState(() {
+                              getTodolist();
+                            });
+                            
+                          });;
+        },
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        actions: [
+        IconButton(onPressed: () {
+          setState(() {
+            getTodolist();
+          });
+        }, icon: Icon(Icons.refresh,color: Colors.white,))
+      ],
+        title: Text('รายการที่จะทำ'),
+      ),
+      body: todolistCreate(),
+    );
+  }
+
+  Widget todolistCreate() {
+    return ListView.builder(
+        itemCount: todolistitems.length,
+        itemBuilder: (context, index) {
+          return Card(
+              child: ListTile(
+            title: Text("${todolistitems[index]['title']}"),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => UpdatePage(
+                          todolistitems[index]['id'],
+                          todolistitems[index]['title'],
+                          todolistitems[index]['detail']))).then((value) {
+                            setState(() {
+                              getTodolist();
+                            });
+                          });
+            },
+          ));
+        });
+  }
+
+  Future<void> getTodolist() async {
+    List alltodo = [];
+    var url = Uri.http('192.168.2.130:8000', '/api/all-todolist');
+    var response = await http.get(url);
+    //var result = json.decode(response.body);
+    var result = utf8.decode(response.bodyBytes);
+    print(result);
+    setState(() {
+      todolistitems = jsonDecode(result);
+    });
+  }
+}
